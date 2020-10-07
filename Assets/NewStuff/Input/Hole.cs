f7a75f3d9f6f5d5bf42a6e24fc7b4d87cd64d3dc
FredrikAlHam <43142824+FredrikAlHam@users.noreply.github.com>
2020-10-07 10:17:48 +0200

    Added the a maximum amount of hits at the same time

---
+++ Assets/NewStuff/Input/Hole.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    [SerializeField] bool isHittable = false;
+    [SerializeField] AudioClip clip;
+    SpriteRenderer sR;
+    [SerializeField] public bool isHit { get; private set; } = false;
+    AudioSource audio = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        sR = GetComponent<SpriteRenderer>();
+    }
+
+    public void Play()
+    {
+        if (clip != null)
+            audio.PlayOneShot(clip);
+    }
+    public void Hit()
+    {
+        Play();
+        isHit = true;
+        sR.color = Color.red;
+    }
+    public void UnHit()
+    {
+        isHit = false;
+        sR.color = Color.white;
+    }
+}
---
+++ Assets/NewStuff/Input/WhackInput.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.ComponentModel.Design;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WhackInput : MonoBehaviour
+{
+    [SerializeField] GameObject[] holes = new GameObject[6];
+    [SerializeField] int maxActiveWhacks;
+    int activeWhacks;
+
+    internal Controls controls;
+
+    void OnEnable()
+    {
+        controls = new Controls();
+        controls.Whacks.Enable();
+        controls.Whacks.Whack1.performed += Whack;
+        controls.Whacks.Whack2.performed += Whack;
+        controls.Whacks.Whack3.performed += Whack;
+        controls.Whacks.Whack4.performed += Whack;
+        controls.Whacks.Whack5.performed += Whack;
+        controls.Whacks.Whack6.performed += Whack;
+
+        controls.Whacks.Whack1.canceled += UnWhack;
+        controls.Whacks.Whack2.canceled += UnWhack;
+        controls.Whacks.Whack3.canceled += UnWhack;
+        controls.Whacks.Whack4.canceled += UnWhack;
+        controls.Whacks.Whack5.canceled += UnWhack;
+        controls.Whacks.Whack6.canceled += UnWhack;
+    }
+    private void OnDisable()
+    {
+        controls.Whacks.Disable();
+        controls.Whacks.Whack1.performed -= Whack;
+        controls.Whacks.Whack2.performed -= Whack;
+        controls.Whacks.Whack3.performed -= Whack;
+        controls.Whacks.Whack4.performed -= Whack;
+        controls.Whacks.Whack5.performed -= Whack;
+        controls.Whacks.Whack6.performed -= Whack;
+
+        controls.Whacks.Whack1.canceled -= UnWhack;
+        controls.Whacks.Whack2.canceled -= UnWhack;
+        controls.Whacks.Whack3.canceled -= UnWhack;
+        controls.Whacks.Whack4.canceled -= UnWhack;
+        controls.Whacks.Whack5.canceled -= UnWhack;
+        controls.Whacks.Whack6.canceled -= UnWhack;
+    }
+
+    private void UnWhack(InputAction.CallbackContext obj)
+    {
+
+        int holeIndex = 0;
+        switch (obj.action.name)
+        {
+            case "Whack 1":
+                holeIndex = 1;
+                break;
+            case "Whack 2":
+                holeIndex = 2;
+                break;
+            case "Whack 3":
+                holeIndex = 3;
+                break;
+            case "Whack 4":
+                holeIndex = 4;
+                break;
+            case "Whack 5":
+                holeIndex = 5;
+                break;
+            case "Whack 6":
+                holeIndex = 6;
+                break;
+        }
+        holeIndex--;
+        Hole hole = holes[holeIndex].GetComponent<Hole>();
+        if (hole.isHit)
+        {
+            activeWhacks--;
+            hole.UnHit();
+            Debug.Log(holeIndex);
+        }
+    }
+
+    private void Whack(InputAction.CallbackContext obj)
+    {
+        if (activeWhacks >= maxActiveWhacks) return;
+        activeWhacks++;
+        int holeIndex = 0;
+        switch (obj.action.name)
+        {
+            case "Whack 1":
+                holeIndex = 1;
+                break;
+            case "Whack 2":
+                holeIndex = 2;
+                break;
+            case "Whack 3":
+                holeIndex = 3;
+                break;
+            case "Whack 4":
+                holeIndex = 4;
+                break;
+            case "Whack 5":
+                holeIndex = 5;
+                break;
+            case "Whack 6":
+                holeIndex = 6;
+                break;
+        }
+        holeIndex--;
+        Hole hole = holes[holeIndex].GetComponent<Hole>();
+        hole.Hit();
+        Debug.Log(holeIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
