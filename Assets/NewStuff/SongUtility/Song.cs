afdee9515c672dbfc8fb7148e60dcbc378b3aeb5
FredrikAlHam <43142824+FredrikAlHam@users.noreply.github.com>
2020-10-07 14:11:30 +0200

    Added Song importing ability Resolves #1

---
+++ Assets/NewStuff/SongUtility/Song.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class Beat
+{
+    public int[] holeIndecies = new int[] { 0, 0, 0, 0, 0, 0 };
+}
+public class Song
+{
+    public Beat[] beats;
+    public string songName;
+    public int timeSig1;
+    public int timeSig2;
+    public int bpm;
+    public AudioClip clip;
+
+    public void Log()
+    {
+        Debug.Log(JsonUtility.ToJson(this));
+
+    }
+    public static Song fromJson(string json)
+    {
+        Song jsonObj = JsonUtility.FromJson<Song>(json);
+        jsonObj.clip = (AudioClip)Resources.Load(jsonObj.songName);
+        //if (jsonObj.clip == null) Debug.LogWarning("song not found"); // funkar ej
+        return jsonObj;
+    }
+}
+
+
---
+++ Assets/NewStuff/SongUtility/SongImporter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SongImporter : MonoBehaviour
+{
+    List<Song> songs = new List<Song>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        //new Song() { beats = new Beat[] { new Beat() } , bpm = 100, songName = "rappasen", timeSig1 = 4, timeSig2 = 4 }.Log();
+
+        foreach (FileInfo fI in new DirectoryInfo(Path.Combine(Application.dataPath, "Resources", "songdata")).GetFiles("*.songdata"))
+        {
+            StreamReader sR = new StreamReader(fI.OpenRead());
+            Song song = Song.fromJson(sR.ReadToEnd());
+            songs.Add(song);
+            song.Log();
+        }
+
+
+        //Song.fromJson("{\"songName\":\"rappasen\",\"timeSig1\":1,\"timeSig2\":2,\"bpm\":100}").Log();
+        //GetComponent<AudioSource>().PlayOneShot(Song.fromJson("{\"songName\":\"rappasen\",\"timeSig1\":1,\"timeSig2\":2,\"bpm\":100}").clip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
