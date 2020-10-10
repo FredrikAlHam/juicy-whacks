75537a668f8e8f589297bd225c57c834afe1d6e8
FredrikAlHam <43142824+FredrikAlHam@users.noreply.github.com>
2020-10-10 14:52:29 +0200

    Update SongTestPlayer.cs
    Improved the SongTestPlayer to display beats when played

--- Assets/NewStuff/SongUtility/SongTestPlayer.cs
+++ Assets/NewStuff/SongUtility/SongTestPlayer.cs
@@ -31,11 +31,11 @@ namespace AudioUtilities
         private void Beat(object sender, ElapsedEventArgs e)
         {
             currentBeat = currentSong.beats[currentBeatIndex];
+            Debug.Log(currentBeat.holeIndecies[currentBeatIndex]);
             currentBeatIndex++;
-            if (currentBeatIndex >= CurrentSong.beats.Length) Stop();
         }
 #pragma warning disable UNT0006 // Incorrect message signature
-        public void Start(Song song)
+        public void Start(Song song) //Must be called on main thread
         {
             Stop();
             currentBeatIndex = 0;
@@ -47,7 +47,7 @@ namespace AudioUtilities
         }
 #pragma warning restore UNT0006 // Incorrect message signature
 
-        public void Stop()
+        public void Stop() //Must be called on main thread
         {
             audio.Stop();
             beatTimer.Stop();
@@ -63,8 +63,12 @@ namespace AudioUtilities
         // Update is called once per frame
         void Update()
         {
+            if (!SongImporter.Ready)
+            {
+                return;
+            }
             //Plays song of whack key index pressed
-            if (SongImporter.Ready && !beatTimer.Enabled)
+            if (!beatTimer.Enabled)
             {
                 if (controls.Whacks.Whack1.triggered) Start(SongImporter.Songs[0]);
                 if (controls.Whacks.Whack2.triggered) Start(SongImporter.Songs[1]);
@@ -73,7 +77,29 @@ namespace AudioUtilities
                 if (controls.Whacks.Whack5.triggered) Start(SongImporter.Songs[4]);
                 if (controls.Whacks.Whack6.triggered) Start(SongImporter.Songs[5]);
             }
-            if (!beatTimer.Enabled) Stop();
+            if (CurrentSong != null)
+            {
+                if (currentBeatIndex >= CurrentSong.beats.Length)
+                {
+                    Stop();
+                }
+                else
+                {
+                    for (int i = 0; i < Holes.holes.Length; i++)
+                    {
+
+                        Hole hole = Holes.holes[i].GetComponent<Hole>();
+                        if (currentBeat.holeIndecies[i] > 0)
+                        {
+                            hole.Hit();
+                        }
+                        else
+                        {
+                            hole.UnHit();
+                        }
+                    }
+                }
+            }
         }
     }
 
