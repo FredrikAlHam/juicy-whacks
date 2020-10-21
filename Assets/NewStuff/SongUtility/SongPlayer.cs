778bbd2f6a033f480e12a8e35db6b76a859b87c1
FredrikAlHam <43142824+FredrikAlHam@users.noreply.github.com>
2020-10-21 09:30:07 +0200

    Pls fix #6 implementation

--- Assets/NewStuff/SongUtility/SongPlayer.cs
+++ Assets/NewStuff/SongUtility/SongPlayer.cs
@@ -23,7 +23,7 @@ namespace AudioUtilities
             beatTimer.Interval = 60000 / Convert.ToDouble(song.bpm)/*ms per beat*/ / 4 /* to sixteenths*/;
             IsPlaying = true;
             Timer delay = new Timer(song.milliesStartDelay);
-            delay.Elapsed += (sender, ctx) => { beatTimer.Start(); delay.Dispose();  };
+            delay.Elapsed += (sender, ctx) => { beatTimer.Start(); delay.Stop(); delay.Dispose(); };
             beatTimer.Elapsed += BeatTimer_Elapsed;
         }
         public void Pause()
