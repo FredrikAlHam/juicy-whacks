bfc2b01c3c2faf9ba3af2820baec6fec03340dbd
FredrikAlHam <43142824+FredrikAlHam@users.noreply.github.com>
2020-10-22 20:48:42 +0200

    Cleaned namespaces and comments

--- Assets/NewStuff/SongUtility/SongImporter.cs
+++ Assets/NewStuff/SongUtility/SongImporter.cs
@@ -1,64 +1,65 @@
-﻿using AudioUtilities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-
-public class SongImporter : MonoBehaviour
+namespace AudioUtilities
 {
-    public static List<Song> Songs { get; private set; } = new List<Song>();
-    public static List<AudioClip> MenuAudioClips { get; private set; } = new List<AudioClip>();
-    public static event Action Complete = new Action(ImportsComplete);
-
-    public static bool Ready { get; private set; } = false;
-    // Start is called before the first frame update
-    void Start()
+    public class SongImporter : MonoBehaviour
     {
-        if (!Ready)
-        {
-            bool x = false;
+        public static List<Song> Songs { get; private set; } = new List<Song>();
+        public static List<AudioClip> MenuAudioClips { get; private set; } = new List<AudioClip>();
+        public static event Action Complete = new Action(ImportsComplete);
 
-            //Gets song assets
-            Addressables.LoadAssetsAsync<Song>("Song", null).Completed += objs =>
+        public static bool Ready { get; private set; } = false;
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (!Ready)
             {
-                foreach (Song song in objs.Result)
+                bool x = false;
+
+                //Gets song assets
+                Addressables.LoadAssetsAsync<Song>("Song", null).Completed += objs =>
                 {
-                    Debug.Log($"Found song {song.songName}");
-                    Songs.Add(song);
-                }
-                if (x)
+                    foreach (Song song in objs.Result)
+                    {
+                        Debug.Log($"Found song {song.songName}");
+                        Songs.Add(song);
+                    }
+                    if (x)
+                    {
+                        Ready = true;
+                        Complete.Invoke();
+                    }
+                    else x = true;
+                };
+                //Gets AudioClips for menu music
+                Addressables.LoadAssetsAsync<AudioClip>("MenuAudio", null).Completed += objs =>
                 {
-                    Ready = true;
-                    Complete.Invoke();
-                }
-                else x = true;
-            };
-            //Gets AudioClips for menu music
-            Addressables.LoadAssetsAsync<AudioClip>("MenuAudio", null).Completed += objs =>
-            {
-                foreach (AudioClip aC in objs.Result)
-                {
-                    MenuAudioClips.Add(aC);
-                }
-                if (x)
-                {
-                    Ready = true;
-                    Complete.Invoke();
-                }
-                else x = true;
-            };
+                    foreach (AudioClip aC in objs.Result)
+                    {
+                        MenuAudioClips.Add(aC);
+                    }
+                    if (x)
+                    {
+                        Ready = true;
+                        Complete.Invoke();
+                    }
+                    else x = true;
+                };
+            }
+
+
+            //foreach (FileInfo fI in new DirectoryInfo(Path.Combine(Application.dataPath, "Resources", "songdata")).GetFiles("*.songdata"))
+            //{
+            //    StreamReader sR = new StreamReader(fI.OpenRead());
+            //    Song song = new Song(sR.ReadToEnd());
+            //    Songs.Add(song);
+            //    song.Log();
+            //}
+
         }
 
-
-        //foreach (FileInfo fI in new DirectoryInfo(Path.Combine(Application.dataPath, "Resources", "songdata")).GetFiles("*.songdata"))
-        //{
-        //    StreamReader sR = new StreamReader(fI.OpenRead());
-        //    Song song = new Song(sR.ReadToEnd());
-        //    Songs.Add(song);
-        //    song.Log();
-        //}
-
+        static void ImportsComplete() => Debug.Log("Imports Complete");
     }
-
-    static void ImportsComplete() => Debug.Log("Imports Complete");
 }
