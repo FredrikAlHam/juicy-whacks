c679d0e186ef0ab956590fdffbdb5c8bf0f3dd67
FredrikAlHam <43142824+FredrikAlHam@users.noreply.github.com>
2020-10-22 23:58:09 +0200

    Added drywetmidi

---
+++ Assets/NewStuff/SongUtility/Midi/MidiPlayer.cs
@@ -0,0 +1,22 @@
+﻿using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Devices;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MidiPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MidiFile.Read(Path.Combine("C:", "testmidi.midi")).GetPlayback(new MidiClockSettings());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
